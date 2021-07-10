a95add2bc2d56a963273a2fc85f778d010b3fbb0
Raul Torres <jr.torsa@gmail.com>
2021-07-09 23:34:22 -0600

    resolviendo el ejercicio

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    chain: ''
+  }
+ 
+  handleChange = (e) => {
+    this.setState({
+      chain: e.target.value,
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" value={this.state.chain} onChange={this.handleChange} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.chain}</p>
       </div>
     );
   }
